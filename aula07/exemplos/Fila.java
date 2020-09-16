044337826d282242f951b8333367a1051de5d906
EmersonPaduan <emerson.paduan@gmail.com>
2020-09-16 16:06:28 -0300

    aula 07 fila

---
+++ aula07/exemplos/Fila.java
@@ -0,0 +1,24 @@
+package exemplos;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class Fila {
+    public static void main(String[] args) {
+        Queue<Integer> fila = new LinkedList<>();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            fila.add( rnd.nextInt(11) ); //adiciona na fila valores aleatórios de 0 à 10
+        }
+
+        System.out.println(fila);
+
+        while (! fila.isEmpty()) {
+            System.out.println( fila.remove() );
+        }
+
+        System.out.println(fila);
+    }
+}
---
+++ aula07/exercicios/Exercicio01.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import java.util.Stack;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Stack<Character> pilha = new Stack<>();
+        String frase = "Este exercício é muito fácil";
+
+        for (int i = 0; i < frase.length(); i++) {
+            pilha.push(frase.charAt(i));
+        }
+
+        while (!pilha.isEmpty()) {
+            System.out.print(pilha.pop());
+        }
+
+        System.out.println();
+
+    }
+}
