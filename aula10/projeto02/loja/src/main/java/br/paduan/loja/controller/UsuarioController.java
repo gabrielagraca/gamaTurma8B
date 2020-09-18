6d22a5ef3aadfdb848ad61fec256a12ce5ba6f86
EmersonPaduan <emerson.paduan@gmail.com>
2020-09-18 17:05:11 -0300

    aula 10 parte 2

--- aula10/projeto02/loja/src/main/java/br/paduan/loja/controller/UsuarioController.java
+++ aula10/projeto02/loja/src/main/java/br/paduan/loja/controller/UsuarioController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.paduan.loja.dao.UsuarioDAO;
@@ -27,6 +29,18 @@ public class UsuarioController {
             return ResponseEntity.ok(usuario);
         }else{
             return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @PostMapping("/usuario/login")
+    public ResponseEntity<Usuario> fazerLogin(@RequestBody Usuario userIncompleto){
+        Usuario userFinded = dao.findByEmailAndSenha(userIncompleto.getEmail(), userIncompleto.getSenha());
+
+        if(userFinded != null){
+            return ResponseEntity.ok(userFinded);
+        }else{
+            return ResponseEntity.status(403).build();
         }
 
     }
--- aula10/projeto02/loja/src/main/java/br/paduan/loja/dao/UsuarioDAO.java
+++ aula10/projeto02/loja/src/main/java/br/paduan/loja/dao/UsuarioDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import br.paduan.loja.model.Usuario;
 
 public interface UsuarioDAO extends CrudRepository<Usuario, Integer> {
-    
+    public Usuario findByEmailAndSenha(String email, String senha);
 }
